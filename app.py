d97c03124ba2110d4cdda9c09de2ef1a05453279
EmpBlitz <adam.shams108@gmail.com>
2026-04-21 17:58:53 -0400

    Add UsrReading model to define tank data structure

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from simulator import readGen
 from storage import openReadings, saveReadings, getTankReading, getAlertReadings, getTankAlert
 
 app = FastAPI()
+
+class UsrReading(BaseModel):
+    tankId = str
+    temperature = float
+    ph = float
+    waterLevel = float

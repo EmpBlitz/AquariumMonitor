1347ba63e35cc8ac4d3bcc904c6d291f57bd726b
EmpBlitz <adam.shams108@gmail.com>
2026-04-21 16:41:17 -0400

    Implement tank data generation and storage functionality

--- simulator.py
+++ simulator.py
@@ -1,2 +1,30 @@
 import random
 import datetime
+
+def readGen(tankId):
+    
+    temperature = round(random.uniform(20.0,30.0), 2)
+    ph = round(random.uniform(6.0,8.5), 2)
+    waterLevel = round(random.uniform(20.0, 50.0), 2)
+    alert = []
+
+    if temperature < 24 or temperature > 27:
+        alert.append(f("Temperature out of range: {temperature} C"))
+
+    if ph < 6.5 or ph > 7.5:
+        alert.append(f("PH out of range: {ph}"))
+
+    if waterLevel < 25:
+        alert.append(f("Low water level: {waterLevel} cm"))
+
+    status = "Healthy" if not alert else "ALERT"
+
+    return {
+        "tankId": tankId,
+        "timestamp": datetime.datetime.now().isoformat(),
+        "temperature": temperature,
+        "ph": ph,
+        "waterLevel": waterLevel,
+        "status": status,
+        "alerts": alert
+    }
--- storage.py
+++ storage.py
@@ -1,2 +1,21 @@
 import json
 from pathlib import Path
+
+DataFile = Path("data/readings.json")
+
+if DataFile.exists():
+    with open(DataFile, "r") as f:
+        json.dump([], f, indent = 4)
+
+
+def openReadings():
+    if not DataFile.exists():
+        return []
+    with open(DataFile, "r") as f:
+        return json.load(f)
+    
+def saveReadings(readings):
+    readings = openReadings()
+    readings.append(readings)
+    with open(DataFile, "w") as f:
+        json.dump(readings, f, indent = 4)
